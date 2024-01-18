4191f6c3d4bbfb82c62170003d31b97752e98683
Jolly <83844571+JollyJolli@users.noreply.github.com>
2024-01-18 12:52:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function irAWeb(enlace) {
+    window.location.href = enlace;
+}
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".loader-wrapper").style.display = "none";
+});
